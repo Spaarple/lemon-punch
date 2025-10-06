81963d98db51c2e40dd0253f9ec512d6e6619998
mlejeune <maximelj44@gmail.com>
2025-10-06 19:05:05 +0200

    feat: newsletter hidden + seo meta

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://lemonpunch.fr',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+  integrations: [sitemap()]
+});
